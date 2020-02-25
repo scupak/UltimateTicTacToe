a43feb6e49f794e93f7bf297a6409886844801b2
scupak <anto1095@easv365.dk>
2020-02-25 11:15:04 +0100

    Update App.java

--- src/ultimatetictactoe/App.java
+++ src/ultimatetictactoe/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        System.out.println("dild");
         stage.show();
     }
 
