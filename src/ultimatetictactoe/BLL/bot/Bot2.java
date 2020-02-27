7e1144c027187a5cfe60b29d0a662232d7679538
Lumby98 <lumby89@gmail.com>
2020-02-27 10:31:14 +0100

    Hideyoshi added

---
+++ src/ultimatetictactoe/BLL/bot/Bot2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimatetictactoe.BLL.bot;
+
+import java.util.List;
+import java.util.Random;
+import ultimatetictactoe.BLL.game.IGameState;
+import ultimatetictactoe.BLL.move.IMove;
+
+/**
+ *
+ * @author lumby
+ */
+public class Bot2 implements IBot{
+    private static final String BOTNAME = "Hideyoshi";
+
+
+    @Override
+    public IMove doMove(IGameState state)
+    {
+        Random ran = new Random();
+        List<IMove> possibleMoves = state.getField().getAvailableMoves();
+        return possibleMoves.get(ran.nextInt(possibleMoves.size()));
+        
+    }
+
+    
+}
